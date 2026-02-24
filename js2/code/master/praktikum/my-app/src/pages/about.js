5734307bc4b52935523e4464681a612db50fbe53
Yan Daffa Putra Liandhie <144084303+YanDaffaPutraLiandhie@users.noreply.github.com>
2026-02-25 00:56:50 +0700

    jobsheet 2

---
+++ js2/code/master/praktikum/my-app/src/pages/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>Nama Mahasiswa : Yan Daffa Putra Liandhie</p>
+            <p>NIM : 2341720142</p>
+            <p>Program Studi : D-IV Teknik Informatika</p>
+        
+        
+        </div>
+    )
+}
