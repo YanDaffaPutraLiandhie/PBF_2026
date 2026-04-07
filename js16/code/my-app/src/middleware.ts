433d493029f7ff3d1c06ef0bf7e817bb913f07fc
Yan Daffa Putra Liandhie <144084303+YanDaffaPutraLiandhie@users.noreply.github.com>
2026-04-07 08:39:35 +0700

    jobsheet 16

---
+++ js16/code/my-app/src/middleware.ts
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+import type { NextRequest, NextFetchEvent } from "next/server";
+import withAuth from "./Middleware/withAuth/withAuth";
+
+function middleware(request: NextRequest, event: NextFetchEvent) {
+  return NextResponse.next();
+}
+
+export default withAuth(middleware, ["/profile", "/produk", "/about", "/admin"]);
+
+export const config = {
+  matcher: ["/profile", "/produk", "/about", "/admin"],
+};
